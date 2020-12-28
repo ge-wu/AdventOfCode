57bf6c272b7be2c8ead29c280e51cd6fc31d38b8
ge-wu <jiayewang65@gmail.com>
2020-12-27 18:31:14 -0600

    Add 2020 day 6

--- 2020/day06/06.py
+++ 2020/day06/06.py
@@ -1,10 +1,44 @@
-ansI = 0
-ansII = 0
-group_size = 0
+def read_file():
+    data = []
+    with open("./06.input") as file:
+        for line in file:
+            data.append(line.strip())
+    return data
 
-cnt = {}
-with open("06.input") as file:
-    for line in file:
-        line = line.strip()
-        print(list(line))
 
+def part1(data):
+    cur = set()
+    ans = 0
+    for d in data:
+        if d != '':
+            cur |= set(list(d))
+        else:
+            ans += len(cur)
+            cur.clear()
+    return ans + len(cur)
+
+
+def part2(data):
+    ans = 0
+    group_size = 0
+    cnt = [0] * 26
+    for d in data:
+        if d != '':
+            group_size += 1
+            for c in d:
+                cnt[ord(c) - 97] += 1
+        else:
+            ans += cnt.count(group_size)
+            cnt = [0] * 26
+            group_size = 0
+    return ans
+
+
+def runner():
+    data = read_file()
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
+
+
+if __name__ == '__main__':
+    runner()
